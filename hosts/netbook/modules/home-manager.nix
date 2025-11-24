35309bfe0fc4d2975bb2f86b8dd10323f28cde75
Vitaliy <vitaliy.jc@yandex.ru>
2025-11-24 18:55:29 +0500

    add HM in nixos

--- hosts/netbook/modules/home-manager.nix
+++ hosts/netbook/modules/home-manager.nix
@@ -1,4 +1,20 @@
-{ inputs, ... }: {
-  imports = [ inputs.home-manager.nixosModules.default ];
+{ config, pkgs, ... }:
+
+let
+  # Тянем Home Manager под релиз 25.05
+  home-manager = builtins.fetchTarball
+    "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+in
+{
+  # Подключаем модуль HM как часть NixOS
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  # Чтобы старые файлы не терялись при перезаписи
   home-manager.backupFileExtension = "backup";
+
+  # Подключаем ТВОЙ уже существующий home-конфиг
+  home-manager.users.vitaliy = import /home/vitaliy/.config/home-manager/home.nix;
+  # путь подставь тот, где реально лежит твой конфиг
 }
