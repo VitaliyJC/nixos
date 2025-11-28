28841bd6c3445e3667f2af24ccbfb97d0c3396fb
Vitaliy <vitaliy.jc@yandex.ru>
2025-11-28 14:43:46 +0500

    add acme cert godaddy

--- hosts/gateway/modules/nginx.nix
+++ hosts/gateway/modules/nginx.nix
@@ -6,15 +6,4 @@
 
     appendHttpConfig = builtins.readFile /home/pipka/nginx/nginx-gateway.conf;
   };
-
-  environment.etc = {
-  "letsencrypt/live/pipka.site-wild/fullchain.pem".source =
-    "/var/lib/acme/pipka.site-wild/fullchain.pem";
-
-  "letsencrypt/live/pipka.site-wild/privkey.pem".source =
-    "/var/lib/acme/pipka.site-wild/key.pem";
-
-  "letsencrypt/live/pipka.site-wild/chain.pem".source =
-    "/var/lib/acme/pipka.site-wild/fullchain.pem";
-};
 }
