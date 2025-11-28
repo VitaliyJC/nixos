a58ef55ccf61aaaa9a8b8a2849bd6574c7fd5ad9
Vitaliy <vitaliy.jc@yandex.ru>
2025-11-28 13:02:24 +0500

    add virtualization

--- hosts/gateway/modules/virtualisation.nix
+++ hosts/gateway/modules/virtualisation.nix
@@ -5,6 +5,6 @@
         storageDriver = "btrfs";
 
         # Рекомендуемые параметры для продакшн/серверов
-        extraOptions = "--log-driver=json-file --log-opt max-size=50m --log-opt max-file=3";
+       # extraOptions = "--log-driver=json-file --log-opt max-size=50m --log-opt max-file=3";
     };
 }
