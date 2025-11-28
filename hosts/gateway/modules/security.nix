46978f84b247d13f3a726288ed3824c30c312217
Vitaliy <vitaliy.jc@yandex.ru>
2025-11-28 14:39:20 +0500

    add acme cert godaddy

--- hosts/gateway/modules/security.nix
+++ hosts/gateway/modules/security.nix
@@ -4,15 +4,18 @@
 
   # Получение сертификатов доменного имени
   security.acme = {
-  acceptTerms = true;
-  defaults.email = "pipon@pipa.com";
+    acceptTerms = true;
+    defaults = {
+      email = "pipon@pipa.com";
+      group = "nginx";  # важная строчка!
+    };
 
-  certs."pipka.site-wild" = {
-    domain = "*.pipka.site";
-    extraDomainNames = [ "pipka.site" ];
-    dnsProvider = "godaddy";
-    credentialsFile = "/root/godaddy-acme.env";
-    dnsPropagationCheck = true;
+    certs."pipka.site-wild" = {
+      domain = "*.pipka.site";
+      extraDomainNames = [ "pipka.site" ];
+      dnsProvider = "godaddy";
+      credentialsFile = "/root/godaddy-acme.env";
+      dnsPropagationCheck = true;
     };
   };
 }
